d5862d16a76df55f618484b953d2f0da7f3e0e0a
Dominika Gnich <d.gnich@wp.pl>
2017-06-11 15:58:27 +0200

    done

---
+++ js/warsztaty.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//var formularz= document.getElementById('formularz');
+
+function pobierzImie(event) {
+    event.preventDefault();
+    console.log("mnscoq");
+
+
+var fname = document.getElementsByName('fname')[0].value;
+var lname = document.getElementsByName('lname')[0].value;
+
+
+console.log(fname);
+    console.log(lname);
+    
+}
